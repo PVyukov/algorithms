aaac9cb5c457c4354eeaf64bbb6ffe703b30f709
pavel.vyukov <vukov87@mail.ru>
2020-04-11 13:13:02 +0300

    L2_HW: 1. added recursion in task 3 and 4        2. have done task 8

--- L2_cycle_rec/less_2_task_3.py
+++ L2_cycle_rec/less_2_task_3.py
@@ -3,13 +3,37 @@
 
 # с циклом
 
+# a = int(input("Enter natural number: "))
+# b = 0
+#
+# while a//10 != 0:
+#     b = (b + a % 10)*10
+#     a = a//10
+# b = b + a % 10
+#
+#
+# print (b)
+
+
+# с рекурсией
+
+
+def reverse(x, k):
+    if x // 10 == 0:
+        return x
+    else:
+        return 10 ** k * (x % 10) + reverse(x // 10, k - 1)
+
+
 a = int(input("Enter natural number: "))
-b = 0
-
-while a//10 != 0:
-    b = (b + a % 10)*10
-    a = a//10
-b = b + a % 10
+cnt = 1
+tmp_a = a
 
 
-print (b)
+while tmp_a // 10 != 0:
+    cnt += 1
+    tmp_a //= 10
+b = reverse(a, cnt - 1)
+print(f'Old number is {a}; new number is {b}')
+
+
--- L2_cycle_rec/less_2_task_4.py
+++ L2_cycle_rec/less_2_task_4.py
@@ -1,6 +1,7 @@
 # 4. Найти сумму n элементов следующего ряда чисел: 1, -0.5, 0.25, -0.125,… Количество элементов (n) вводится
 # с клавиатуры.
 
+# с циклом
 num = int(input("Enter natural number: "))
 my_sum = 1
 next_el = 1
@@ -10,6 +11,18 @@ while k < num:
     next_el = next_el / 2
     my_sum += pow(-1, k) * next_el
     k += 1
-    print(f'next is {next_el}, summ is {my_sum}')
+print(my_sum)
 
+# с рекурсией
+
+
+def func(a, cnt, stop):
+    if cnt == stop:
+        return a
+    else:
+        return a + func(-1 * a / 2, cnt + 1, stop)
+
+
+num = int(input("Enter quantity of elements: "))
+my_sum = func(1, 1, num)
 print(my_sum)
