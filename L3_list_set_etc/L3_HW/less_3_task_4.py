37d254d8948adbf3da1cf4e745a0d51ac23db2b2
pavel.vyukov <vukov87@mail.ru>
2020-04-16 08:42:55 +0300

    All HW3 + web L4

--- L3_list_set_etc/L3_HW/less_3_task_4.py
+++ L3_list_set_etc/L3_HW/less_3_task_4.py
@@ -8,15 +8,32 @@ array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
 max_cnt = 0
 
-
 for num_1 in array:
     cnt = 0
     for num_2 in array:
         if num_1 == num_2:
             cnt += 1
-#    print(f'{num_1:>3}:\t{cnt}')
+    #    print(f'{num_1:>3}:\t{cnt}')
     if max_cnt < cnt:
         max_cnt = cnt
         most_freq = num_1
 print(f'{most_freq}')
 
+# ----вариант учителя--------
+# counter = {}
+# frequency = 1
+# num = None
+# for item in array:
+#     if item in counter:
+#         counter[item] += 1
+#     else:
+#         counter[item] = 1
+#
+#     if counter[item] > frequency:
+#         frequency = counter[item]
+#         num = item
+#
+# if num is not None:
+#     print(f'Число {num} встречется {frequency} раз(а)')
+# else:
+#     print('Все элементы уникальны')
--- L3_list_set_etc/L3_HW/less_3_task_5.py
+++ L3_list_set_etc/L3_HW/less_3_task_5.py
@@ -1,3 +1,8 @@
+# В массиве найти максимальный отрицательный элемент. Вывести на экран его значение и позицию в массиве.
+# Примечание к задаче: пожалуйста не путайте «минимальный» и «максимальный отрицательный». Это два
+# абсолютно разных значения.
+
+
 import random
 
 SIZE = 10
@@ -5,6 +10,9 @@ MIN_ITEM = -10
 MAX_ITEM = 10
 array = [random.randint(MIN_ITEM, MAX_ITEM) for _ in range(SIZE)]
 print(array)
+
+
+# ----не верно, т.к. если первый элемент сразу берется как максимальный, н он может быть >0 -------
 index = 0
 max_el = array[0]
 
@@ -15,3 +23,30 @@ for i, el in enumerate(array[1:], 1):
             index = i
 
 print(f'max el is {max_el}; index is {index}')
+
+
+# ----вариант учителя--------
+# вариант 1
+# i = 0
+# index = -1
+# while i < len(array):   # или for i in range(len(array)):
+#     if array[i] < 0 and index == -1:
+#         index = i
+#     elif 0 > array[i] > array[index]:
+#         index = i
+#     i += 1
+#
+# if index != -1:
+#     print(f'Максимальное отрицательное число {array[index]} '
+#           f'находится в ячейке {index}')
+#
+# # вариант 2
+# num = float('-inf')
+# for i, item in enumerate(array):
+#     if 0 > item > num:
+#         num = item
+#         index = i
+#
+# if num != float('-inf'):
+#     print(f'Максимальное отрицательное число {num} '
+#           f'находится в ячейке {index}')
